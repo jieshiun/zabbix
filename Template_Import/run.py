086da6ff2c04eb2036fd853527f48fb4b9069f87
ivan_cheng <ivan_cheng@weleader.com.tw>
2022-08-25 17:54:39 +0800

    fixed text

--- Template_Import/run.py
+++ Template_Import/run.py
@@ -5,4 +5,4 @@ import subprocess
 for path, subdirs, files in os.walk("/home/administrator/templates"):
     for name in files:
         if name.endswith('.yaml'):
-            subprocess.call("./zabbix_template_import.py --url http://localhost/zabbix/api_jsonrpc.php --user Admin --password {your_password} template_app_activemq_jmx.yaml", shell=True)
+            subprocess.call("./zabbix_template_import.py --url http://localhost/zabbix/api_jsonrpc.php --user Admin --password {your_password} " + os.path.join(path, name), shell=True)
