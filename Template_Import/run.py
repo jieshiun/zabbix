768919785a0d17e3ea692dd2d1ed46adfa98226a
ivan_cheng <ivan_cheng@weleader.com.tw>
2022-08-25 17:36:11 +0800

    Add the batch script for import templates

---
+++ Template_Import/run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+
+for path, subdirs, files in os.walk("/home/administrator/templates"):
+    for name in files:
+        if name.endswith('.yaml'):
+            subprocess.call("./zabbix_template_import.py --url http://localhost/zabbix/api_jsonrpc.php --user Admin --password {your_password} template_app_activemq_jmx.yaml", shell=True)
