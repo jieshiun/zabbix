6b4bef8a0a6d9f785b0cccff225b9d95b40a7664
ivan_cheng <ivan_cheng@weleader.com.tw>
2022-08-25 16:22:34 +0800

    Add import template script

---
+++ Template_Import/zabbix_template_import.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+import argparse
+from urllib import request
+import json
+import sys
+from pprint import pformat
+import os
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+def zbxrequest(url, method, auth, params):
+
+    if params is None:
+        params = {}
+    data = { "jsonrpc": "2.0", "id": 1, "method": method, "auth": auth, "params": params }
+    # Convert to string and then to byte
+    data = json.dumps(data).encode('utf-8')
+    req = request.Request(args.url, headers={'Content-Type': 'application/json'}, data=data)
+    resp = request.urlopen(req)
+    # Get string
+    resp = resp.read().decode('utf-8')
+    # Convert to object
+    resp = json.loads(resp)
+    return resp
+
+
+try:
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Import XML configuration files using Zabbix API')
+    parser.add_argument('template_file')
+    parser.add_argument('-u', '--user', required=True, help='user name')
+    parser.add_argument('-p', '--password', '--pass', required=True, help='password', metavar='PASSWORD')
+    parser.add_argument('-s', '--url', default='http://127.0.0.1:80/api_jsonrpc.php',
+                        help='Zabbix API URL, default is http://127.0.0.1:80/api_jsonrpc.php')
+    args = parser.parse_args()
+
+    # Get authentication token
+    # https://www.zabbix.com/documentation/6.0/en/manual/api/reference/user/login
+    auth_result = zbxrequest(args.url, method="user.login", auth=None,
+                             params={"user": args.user, "password": args.password})
+
+    # If authentication was not OK
+    if 'result' not in auth_result:
+        raise Exception('ERROR: auth failed\n' + pformat(auth_result))
+
+    auth_token = auth_result['result']
+
+    # Read template file content
+    with open(args.template_file, 'r', encoding='utf-8') as f:
+        source = f.read()
+
+    # Set import parameters, including template file content
+    # https://www.zabbix.com/documentation/6.0/en/manual/api/reference/configuration/import
+    params = {'format': 'yaml',
+              'rules': {'groups': {'createMissing': True},
+                        'hosts': {'createMissing': True, 'updateExisting': True},
+                        'items': {'createMissing': True, 'updateExisting': True},
+                        'templates': {'createMissing': True, 'updateExisting': True},
+                        'templateLinkage': {'createMissing': True},
+                        'discoveryRules': {'createMissing': True, 'updateExisting': True},
+                        'triggers': {'createMissing': True, 'updateExisting': True},
+                        'graphs': {'createMissing': True, 'updateExisting': True},
+                        'valueMaps': {'createMissing': True},
+                        'images': {'createMissing': True, 'updateExisting': True},
+                        'maps': {'createMissing': True, 'updateExisting': True},
+                        },
+              'source': source
+              }
+
+    import_result = zbxrequest(args.url, method="configuration.import", auth=auth_token, params=params)
+    # Something like: {'id': 1, 'jsonrpc': '2.0', 'result': True}
+
+    if 'result' in import_result and import_result['result']:
+        print('SUCCESS: configuration import')
+    else:
+        raise Exception('ERROR: configuration import failed\n' + pformat(import_result))
+
+    exit_code = 0
+
+except Exception as e:
+
+   print(str(e), file=sys.stderr)
+   exit_code=1
+
+finally:
+    # Logout to prevent generation of unnecessary open sessions
+    #https://www.zabbix.com/documentation/6.0/en/manual/api/reference/user/logout
+    if 'auth_token' in vars():
+        zbxrequest(args.url, method="user.logout", auth=auth_token, params={})
+
