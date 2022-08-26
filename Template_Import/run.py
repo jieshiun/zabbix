9ddcc7f630b47093b6b980c20303f973b83439b0
ivan_cheng <ivan_cheng@weleader.com.tw>
2022-08-26 10:58:58 +0800

    Add print() to show template name

--- Template_Import/run.py
+++ Template_Import/run.py
@@ -5,4 +5,5 @@ import subprocess
 for path, subdirs, files in os.walk("/home/administrator/templates"):
     for name in files:
         if name.endswith('.yaml'):
+            print(os.path.join(path, name))
             subprocess.call("./zabbix_template_import.py --url http://localhost/zabbix/api_jsonrpc.php --user Admin --password {your_password} " + os.path.join(path, name), shell=True)
