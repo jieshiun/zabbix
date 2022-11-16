6e0355e68de7fd17d83074b2ad5766ea42f37658
ivan_cheng <ivan_cheng@weleader.com.tw>
2022-11-16 17:35:12 +0800

    Add chi conf and powershell

---
+++ Scripts/chi/get_backup_status.ps1
@@ -0,0 +1,25 @@
+﻿param(
+     [Parameter()]
+     [string]$backupPath
+ )
+
+#$backupPath = 'E:\Autobackup\'
+$backupPath = $backupPath.Trim("'")
+
+$date = (get-date).AddDays(-1).ToString("yyyMMdd")
+$dbs = 'Sys','01','02','03'
+$status = '0','0','0','0'
+
+for ($index = 0; $index -lt $dbs.count; $index++)
+{
+    $fileName = "CHI" + $date + "." + $dbs[$index]
+    $sourceFile = $backupPath + $fileName
+    #Write-Host $sourceFile
+
+    #If the file is exist, backup success
+    if ((Test-Path -Path $sourceFile -PathType Leaf)) {
+        $status[$index] = 1
+    }
+}
+
+[ordered]@{sys=$status[0];company1=$status[1];company2=$status[2];company3=$status[3]} | ConvertTo-Json -Compress
